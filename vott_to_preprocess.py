b793874285b751f82719cf4dab33944cb390350e
Nathaniel Sprecher <sprechernd1@gcc.edu>
2020-05-05 12:26:47 -0400

    fixed minor bug in file naming

--- vott_to_preprocess.py
+++ vott_to_preprocess.py
@@ -31,11 +31,27 @@ def main(argv):
             curName = curName.replace("\"","")
             print("line "+str(i)+". Name: "+curName)
 
+            '''if(len(curName.split("#t=")) > 1):
+                time = curName.split("#t=")[1].split(".jpg")[0]
+                timeInt = time.split(".")[0]
+
+                if(len(time.split(".")) > 1):
+                    timeDec = time.split(".")[1]
+                else:
+                    timeDec = ''
+                timeDiff = 6 - len(timeDec)
+                if(timeDiff > 0):
+                    while(timeDiff > 0):
+                        timeDiff = timeDiff-1
+                        timeDec = timeDec + '0'
+                
+                curName = timeInt + '.' + timeDec + '.jpg''''
+
             #change name of image
             newName = str(i)+'.jpg'
             csvArr[i][4] = newName
             #change corresponding name of image
-            os.rename('train/images/'+curName, 'train/images/'+newName)
+            #os.rename('train/images/'+curName, 'train/images/'+newName)
             i = i+1
     
     np.savetxt("data.csv", csvArr, delimiter=",", fmt='%s')
